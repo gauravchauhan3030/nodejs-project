d50780507c5463cccd30263d3483e80a996bd214
Gaurav <gauravchauhan3030@gmail.com>
2022-06-27 18:44:51 +0530

    added simple static routes and their html page

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.use(shopRoutes);
+app.use("/admin", adminRoutes);
 
 app.listen(3000, () => {
   console.log("running on 3000");
