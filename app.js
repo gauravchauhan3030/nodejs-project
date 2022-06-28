3c9cffa468df50951b76b7f49a6c9c872b4fee82
Gaurav <gauravchauhan3030@gmail.com>
2022-06-28 16:20:10 +0530

    dynamic content completed

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const shopRoutes = require("./routes/shop");
-const adminRoutes = require("./routes/admin");
+const expressHbs = require("express-handlebars");
 
 const app = express();
+// const hbs = expressHbs.create({
+//   defaultLayout: "layout",
+//   extname: ".hbs",
+// });
+
+// app.engine("hbs", hbs.engine);
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
-app.use("/admin", adminRoutes);
 
-app.listen(3000, () => {
-  console.log("running on 3000");
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
+
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
-const express = require("express");
 const path = require("path");
+
+const express = require("express");
 
 const rootDir = require("../util/path");
 
@@ -7,13 +8,22 @@ const router = express.Router();
 
 const products = [];
 
-router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
 });
 
-router.post("/add-product", (req, res) => {
-  console.log(req.body);
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
